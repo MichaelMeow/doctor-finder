fcd84758d8f2f69cbd16f0830b41947dd607ab0d
Student <example@example.com>
2018-10-12 13:38:46 -0700

    stop using tests because not working with asynchrony

--- spec/medicalissue-spec.js
+++ spec/medicalissue-spec.js
@@ -2,6 +2,14 @@ import MedicalIssue from './../src/medicalissue.js';
 
 describe('MedicalIssue', function() {
 
+  beforeEach(function() {
+    jasmine.clock().install();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
   it('should create an object constructor', function() {
     const issue = new MedicalIssue("symptom");
     expect(issue.symptom).toEqual("symptom")
@@ -29,11 +37,13 @@ describe('MedicalIssue', function() {
       const body = JSON.parse(response);
       const doctorName = body.data[0].practices[0].name;
       result = doctorName;
-      expect(result).toEqual("Richard Ashton, DMD")
     }, function(error) {
       const errorString = `There was an error processing your request: ${error.message}`;
       return errorString;
     });
+    jasmine.clock().tick(6001);
+    expect(result).toEqual("Richard Ashton, DM")
   })
+
 
 });
