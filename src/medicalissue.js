b474e2d211520560ebb76694c191b38a5eba7e24
Student <example@example.com>
2018-10-12 13:46:32 -0700

    separate logic

--- src/medicalissue.js
+++ src/medicalissue.js
@@ -4,7 +4,21 @@ export default class MedicalIssue {
     this.symptom = symptom;
   }
 
-
+  getDoctor(symptom) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=1&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 
 
 
