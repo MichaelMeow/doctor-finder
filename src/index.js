0c2c543177cc4b4db4e4e0f57379a7ccfea3861b
Student <example@example.com>
2018-10-12 14:24:40 -0700

    add more search results to symptom search

--- src/index.js
+++ src/index.js
@@ -17,8 +17,16 @@ $(document).ready(function(){
       const body = JSON.parse(response);
       const dataArray = body.data;
       for (var i = 0; i < dataArray.length; i++) {
-        const result = dataArray[i].practices[0].name;
-        $(".results").append("<li>" + result + "</li>");
+        $(".results").append("<ul>" + i);
+        $(".results").append("<li>First Name: " + dataArray[i].profile.first_name + "</li>");
+        $(".results").append("<li>Last Name: " + dataArray[i].profile.last_name + "</li>");
+        $(".results").append("<li>Phone: " + dataArray[i].practices[0].phones[0].number + "</li>");
+        $(".results").append("<li>Address: " + dataArray[i].practices[0].visit_address.street + "</li>");
+        if (dataArray[i].practices[0].website){
+          $(".results").append("<li>Website: " + dataArray[i].practices[0].website + "</li>");
+        }
+        $(".results").append("<li>Accepting New Patients?: " + dataArray[i].practices[0].accepts_new_patients + "</li>");
+        $(".results").append("</ul>");
       }
     }, function(error) {
       const errorString = `There was an error processing your request: ${error.message}`;
@@ -36,18 +44,18 @@ $(document).ready(function(){
       $(".doctorSearch").html("");
       const body = JSON.parse(response);
       const dataArray = body.data;
-      for (var i = 0; i < dataArray.length; i++) {
+      for (var j = 0; j < dataArray.length; j++) {
 
-        $(".doctorSearch").append("<ul>" + i);
-        $(".doctorSearch").append("<li>First Name: " + dataArray[i].profile.first_name + "</li>");
-        $(".doctorSearch").append("<li>Last Name: " + dataArray[i].profile.last_name + "</li>");
-        $(".doctorSearch").append("<li>Phone: " + dataArray[i].practices[0].phones[0].number + "</li>");
-        $(".doctorSearch").append("<li>Address: " + dataArray[i].practices[0].visit_address.street + "</li>");
+        $(".doctorSearch").append("<ul>" + j);
+        $(".doctorSearch").append("<li>First Name: " + dataArray[j].profile.first_name + "</li>");
+        $(".doctorSearch").append("<li>Last Name: " + dataArray[j].profile.last_name + "</li>");
+        $(".doctorSearch").append("<li>Phone: " + dataArray[j].practices[0].phones[0].number + "</li>");
+        $(".doctorSearch").append("<li>Address: " + dataArray[j].practices[0].visit_address.street + "</li>");
 
-        if (dataArray[i].practices[0].website){
-          $(".doctorSearch").append("<li>Website: " + dataArray[i].practices[0].website + "</li>");
+        if (dataArray[j].practices[0].website){
+          $(".doctorSearch").append("<li>Website: " + dataArray[j].practices[0].website + "</li>");
       }
-        $(".doctorSearch").append("<li>Accepting New Patients?: " + dataArray[i].practices[0].accepts_new_patients + "</li>");
+        $(".doctorSearch").append("<li>Accepting New Patients?: " + dataArray[j].practices[0].accepts_new_patients + "</li>");
 
         $(".doctorSearch").append("</ul>");
       }
