e5ace22bcf580ec81aba28e3368b5c7d75ccf6ea
Student <example@example.com>
2018-10-12 12:52:21 -0700

    fix first test descriptor

--- spec/medicalissue-spec.js
+++ spec/medicalissue-spec.js
@@ -2,18 +2,11 @@ import MedicalIssue from './../src/medicalissue.js';
 
 describe('MedicalIssue', function() {
 
-  it('should create an xmlhttprequest from the betterdoctor API', function() {
+  it('should create an object constructor', function() {
     // perform function and test for result
     const issue = new MedicalIssue("symptom");
     expect(issue.symptom).toEqual("symptom")
   });
-  // it('should create an asynchronous request', function() {
-  //   // perform function
-  //   start timer in Javascript after promise
-  //   finish request
-  //   stop timer
-  //   test that output time taken larger than zero
-  //   expect().toEqual()
-  // });
+
 
 });
